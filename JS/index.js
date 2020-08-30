5c9308add764473976c1e0db67b0ad635bc84ba4
Arsen <a.mamataliev93@gmail.com>
2020-08-30 14:07:00 +0600

    added login and Sign up pages

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,4 @@
-
-
+//To close|open Menu 
 function closeFn() {
     var x = document.getElementById("menuBar");
     if (x.style.display === "block") {
@@ -7,4 +6,35 @@ function closeFn() {
     } else {
       x.style.display = "block";
     }
+  }
+  //Open register Box
+  function openRegisterBox(){
+    let registerBox = document.getElementsByClassName("register-box")[0];
+    registerBox.style.display = "block";
+  }
+
+    //Close register Box
+  function closeRegisterBox(){
+    let registerBox = document.getElementsByClassName("register-box")[0];
+    registerBox.style.display = "none";
+  } 
+  //Close Login box
+  function closeLoginBox(){
+    let loginBox = document.getElementsByClassName("login-box")[0];
+    loginBox.style.display = "none";
+  }
+  //Open Login box
+  function openLoginBox(){
+    let loginBox = document.getElementsByClassName("login-box")[0];
+    loginBox.style.display = "block";
+  }
+
+  function openLoginBoxFromRegisBox(){
+    openLoginBox();
+    closeRegisterBox();
+  }
+
+  function openRegisBoxFromLoginBox(){
+    closeLoginBox();
+    openRegisterBox()
   }
